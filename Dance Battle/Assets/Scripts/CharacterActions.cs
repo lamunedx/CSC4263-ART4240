0e67e47bd0136e2d1c29a8db0d0f1fdbfb88d3c5
Guang Li <guangli7@gmail.com>
2017-03-11 16:39:58 -0600

    Added script to animation
    Added Script to control when each animation would work

--- Dance Battle/Assets/Scripts/CharacterActions.cs
+++ Dance Battle/Assets/Scripts/CharacterActions.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class CharacterActions : MonoBehaviour {
 
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		action ();
+	}
+
+	void action(){
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			anim.SetBool ("walk",true);
+		} 
+		else 
+		{
+			anim.SetBool ("walk",false);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			anim.SetBool ("jump",true);
+		} 
+		else 
+		{
+			anim.SetBool ("jump",false);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			anim.SetBool ("crouch",true);
+		} 
+		else 
+		{
+			anim.SetBool ("crouch",false);
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			anim.SetBool ("punch",true);
+		} 
+		else 
+		{
+			anim.SetBool ("punch",false);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			anim.SetBool ("kick",true);
+		} 
+		else 
+		{
+			anim.SetBool ("kick",false);
+		}
+
 	}
 }
