ec8b3af9ed4848cb8cfd2e84562abdff13a7e111
Wilson <wzhu7148@gmail.com>
2017-03-14 21:20:35 -0500

    added sample animation
    todo: fix animation lag upon input release & add more animations

---
+++ Dance Battle/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script file will allow users to control their character movement
+public class PlayerControl : MonoBehaviour {
+
+  Animator animate;
+	
+	void Start () {
+    animate = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+    KeyInput();
+    {
+      float move = Input.GetAxis("Horizontal");
+      animate.SetFloat("Speed", move);
+    }
+
+	}
+
+  void KeyInput() {
+    if(Input.GetKey(KeyCode.RightArrow)) {
+      transform.Translate(Vector2.right * 3f * Time.deltaTime);
+      transform.eulerAngles = new Vector2(0, 0);
+    }
+
+    if (Input.GetKey(KeyCode.LeftArrow)) {
+      transform.Translate(Vector2.right * -3f * Time.deltaTime);
+      transform.eulerAngles = new Vector2(0, 0);
+    }
+  }
+}
