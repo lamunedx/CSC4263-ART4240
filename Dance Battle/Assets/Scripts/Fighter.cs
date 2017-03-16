6e30dfbc78d46e58a29b58e7bb6079a4c0124277
Sandeep Ingale <sandeepingale.18@gmail.com>
2017-03-15 21:26:11 -0500

    Physics and script added
    Physics and script is added for a player. I am writing fighter code.

---
+++ Dance Battle/Assets/Scripts/Fighter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour {
+
+    public static float MAX_HEALTH = 100f;
+
+    public float health = MAX_HEALTH;
+    public string fightername;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public float healthPercent {
+        get {
+
+        }
+    }
+}
