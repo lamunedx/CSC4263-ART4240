7077494675a99bca5747d774544fe49675ab7b0b
MahdJudeh <mjudeh1@lsu.edu>
2017-03-20 22:42:04 -0500

    basic song analysis is possible. Need to write to outer txt file

--- Dance Battle/Assets/Scripts/MusicGenerator.cs
+++ Dance Battle/Assets/Scripts/MusicGenerator.cs
@@ -1,16 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MusicGenerator : MonoBehaviour {
+    AudioSource song;
+    public GameObject cube;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float[] samples = new float[512];
+    public float[] frequencyRange = new float[8];
+    public List<float> timeStampsOfBeats = new List<float>();
+    Boolean checkWrite = false;
+    
+    // Use this for initialization
+    void Start() {
+        song = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        getSpectrumAudioSource();
+        getfrequencyrange();
+        if(frequencyRange[0] > 25)
+        {
+            timeStampsOfBeats.Add(song.time);
+        }
+        if(Time.time > 20 && checkWrite == false)
+        {
+            saveToFile();
+        }
+    }
+
+    void getSpectrumAudioSource()
+    {
+        song.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void getfrequencyrange()
+    {
+        int currentsample = 0;
+        for (int i = 0; i < frequencyRange.Length; i++)
+        {
+            float average = 0;
+            int numberofsamples = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                numberofsamples += 2;
+            }
+            for (int j = 0; j < numberofsamples; j++)
+            {
+                average += samples[currentsample] * (2);
+                currentsample++;
+            }
+            average /= currentsample;
+            frequencyRange[i] = average *100;
+
+        }
+    }
+    void saveToFile()
+    {
+        System.IO.File.AppendAllText(Application.dataPath + "/newText.txt", timeStampsOfBeats.ToString());
+        Debug.Log("Write = true");
+        checkWrite = true;
+    }
 }
