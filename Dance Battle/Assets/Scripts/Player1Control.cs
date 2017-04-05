06c797111023ad6d8730d7fabf3bd3461f71389f
Wilson <wzhu7148@gmail.com>
2017-04-05 14:28:32 -0500

    Revert "player 2, and wilsons animations"
    This reverts commit d0c66c5425f0c8d11a9c4f85a74a40c085289f92.

--- Dance Battle/Assets/Scripts/Player1Control.cs
+++ Dance Battle/Assets/Scripts/Player1Control.cs
@@ -24,41 +24,27 @@ public class Player1Control : MonoBehaviour {
       animator.Play("JumpingUp");
       OnGround = false;
     }
-	if (!OnGround) {
-		if (body.velocity.y < 1 && body.velocity.y >= 0) {
-			animator.Play ("Jumping");
-		} 
-		else if (body.velocity.y > 1) {
-			animator.Play ("JumpHang");
-		} 
-		else {
-			animator.Play ("JumpingDown");
-		}
-	}
-    //else if(!OnGround && body.velocity.y != 0) {
-    //  animator.Play("Jumping");
-    //}
-    //else if(!OnGround && body.velocity.y == 0) {
-    //  animator.Play("JumpHang");
-    //}
-    //else if(Input.GetKey(KeyCode.UpArrow) && OnGround) {
-    //  animator.Play("JumpingDown");
-    //  animator.Play("Idle");
-    //}
-	if (Input.GetKey(KeyCode.DownArrow) && !Crouched && OnGround) {
+    else if(!OnGround && body.velocity.y != 0) {
+      animator.Play("Jumping");
+    }
+    else if(!OnGround && body.velocity.y == 0) {
+      animator.Play("JumpHang");
+    }
+    else if(Input.GetKey(KeyCode.UpArrow) && OnGround) {
+      animator.Play("JumpingDown");
+      animator.Play("Idle");
+    }
+    if (Input.GetKey(KeyCode.DownArrow) && !Crouched) {
       animator.Play("CrouchingDown");
       Crouched = true;
     }
-    else if(Input.GetKey(KeyCode.DownArrow) && Crouched && OnGround) {
+    else if(Input.GetKey(KeyCode.DownArrow) && Crouched) {
       animator.Play("Crouching");
     }
-    else if (Input.GetKeyUp(KeyCode.DownArrow) && Crouched && OnGround) {
+    else if (Input.GetKeyUp(KeyCode.DownArrow) && Crouched) {
       animator.Play("CrouchingUp");
-	  Crouched = false;
+      animator.Play("Idle");
     }
-	if (!Crouched && OnGround) {
-		animator.Play ("Idle");
-	}
   }
 
   private void OnCollisionEnter2D(Collision2D collision) {
