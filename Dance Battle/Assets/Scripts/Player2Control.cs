4e8b80b0d630c9d435b9eddad68aa77e03240849
Wilson <wzhu7148@gmail.com>
2017-04-17 13:36:06 -0500

    tweaked animations

--- Dance Battle/Assets/Scripts/Player2Control.cs
+++ Dance Battle/Assets/Scripts/Player2Control.cs
@@ -7,91 +7,83 @@ public class Player2Control : MonoBehaviour {
 
   private Animator anime;
   private Rigidbody2D rigid;
-	private CapsuleCollider2D[] cap;
+  private CapsuleCollider2D[] cap;
 
-  // reference to the other player
-  public GameObject otherPlayer;
-  private Rigidbody2D otherRigid;
-
-  void Start() {
+	void Start () {
     anime = GetComponent<Animator>();
     rigid = GetComponent<Rigidbody2D>();
-		cap = GetComponents<CapsuleCollider2D>();
+    cap = GetComponents<CapsuleCollider2D>();
+	}
 
-    otherRigid = otherPlayer.GetComponent<Rigidbody2D>();
-  }
-
-  void Update() {
+	void Update () {
     // movement keys -----------------------------------------------------------
-		if (Input.GetKey(KeyCode.D) && !anime.GetBool("Crouch")) {
+    if (Input.GetKey(KeyCode.RightArrow) && !anime.GetBool("crouching")) {
       transform.Translate(Vector2.right * 2f * Time.deltaTime);
-      anime.SetBool("Speed", true);
+      anime.SetBool("moving", true);
     }
-    if (Input.GetKeyUp(KeyCode.D)) {
-      anime.SetBool("Speed", false);
+    if (Input.GetKeyUp(KeyCode.RightArrow)) {
+      anime.SetBool("moving", false);
     }
 
-		if (Input.GetKey(KeyCode.A) && !anime.GetBool("Crouch")) {
+	  if (Input.GetKey(KeyCode.LeftArrow) && !anime.GetBool("crouching")) {
       transform.Translate(Vector2.left * 2f * Time.deltaTime);
-      anime.SetBool("Speed", true);
+      anime.SetBool("moving", true);
     }
-    if (Input.GetKeyUp(KeyCode.A)) {
-      anime.SetBool("Speed", false);
+    if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+      anime.SetBool("moving", false);
     }
 
-    if (Input.GetKey(KeyCode.S) && anime.GetBool("Ground")) {
-      cap[0].enabled = false;
-      cap[1].enabled = true;
+	  if (Input.GetKey(KeyCode.DownArrow) && anime.GetBool("onGround")) {
+      //cap[0].enabled = false;
+      //cap[1].enabled = true;
       //GetComponentInChildren<EdgeCollider2D>().enabled = false;
-      transform.position = new Vector2(transform.localPosition.x, -3.24f);
-      anime.SetBool("Crouch", true);
+      //transform.position = new Vector2(transform.localPosition.x, -3.24f);
+	    anime.SetBool("crouching", true);
     }
-    if (Input.GetKeyUp(KeyCode.S)) {
+    if (Input.GetKeyUp(KeyCode.DownArrow)) {
       cap[1].enabled = false;
       cap[0].enabled = true;
       //GetComponentInChildren<EdgeCollider2D>().enabled = true;
       transform.position = new Vector2(transform.localPosition.x, -2.96f);
-      anime.SetBool("Crouch", false);
+	    anime.SetBool("crouching", false);
     }
 
-    if (Input.GetKeyDown(KeyCode.W) && anime.GetBool("Ground")) {
-			rigid.AddForce(Vector2.up * 550f);
-		}
+	  if (Input.GetKeyDown(KeyCode.UpArrow) && anime.GetBool("onGround")) {
+      rigid.AddForce(Vector2.up * 550f);
+    }
 
     // action keys -------------------------------------------------------------
-    if (Input.GetKeyDown(KeyCode.Z)) {
-      anime.SetBool("Punch", true);
-      cap[2].enabled = true;
-      cap[2].isTrigger = true;
+	  if (Input.GetKeyDown(KeyCode.I) && !anime.GetBool("moving")) {
+      anime.SetBool("punching", true);
+      //cap[2].enabled = true;
+      //cap[2].isTrigger = true;
     }
-    if (anime.GetCurrentAnimatorStateInfo(0).IsName("Punch")) {
-      anime.SetBool("Punch", false);
-      cap[2].enabled = false;
+	  if (anime.GetCurrentAnimatorStateInfo(0).IsName("punch")) {
+      anime.SetBool("punching", false);
+      //cap[2].enabled = false;
     }
-
-    // make sure the player is facing the right direction
-    // note: this handles both players, so don't add this to the player 1 script
-    if (rigid.position.x > otherRigid.position.x) {
-      rigid.transform.localScale = new Vector2(1f, rigid.transform.localScale.y);
-      otherRigid.transform.localScale = new Vector2(-1f, otherRigid.transform.localScale.y);
-    }
-    else {
-      rigid.transform.localScale = new Vector2(-1f, rigid.transform.localScale.y);
-      otherRigid.transform.localScale = new Vector2(1f, otherRigid.transform.localScale.y);
-    }
+	  if (Input.GetKeyDown(KeyCode.O) && !anime.GetBool("moving")) {
+	    anime.SetBool("kicking", true);
+	  }
+	  if (anime.GetCurrentAnimatorStateInfo(0).IsName("kick")) {
+	    anime.SetBool("kicking", false);
+	  }
   }
 
   private void OnCollisionEnter2D(Collision2D collision) {
     if (collision.gameObject.name == "Platform") {
-      anime.SetBool("Ground", true);
-      anime.SetBool("Jump", false);
+	  anime.SetBool("onGround", true);
+      anime.SetBool("jumping", false);
+    }
+    if (collision.gameObject.name == "Player2") {
+      anime.SetBool("jumping", false);
     }
   }
 
   private void OnCollisionExit2D(Collision2D collision) {
     if (collision.gameObject.name == "Platform") {
-      anime.SetBool("Ground", false);
-      anime.SetBool("Jump", true);
+	  anime.SetBool("onGround", false);
+      anime.SetBool("jumping", true);
     }
   }
 }
