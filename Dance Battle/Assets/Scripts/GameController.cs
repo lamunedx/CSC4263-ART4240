3ef1946dcb9d25cac67cc8919b715f66fd705b0e
MahdJudeh <mjudeh1@lsu.edu>
2017-05-03 22:59:15 -0500

    fixed background

--- Dance Battle/Assets/Scripts/GameController.cs
+++ Dance Battle/Assets/Scripts/GameController.cs
@@ -1,33 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.VersionControl;
-using UnityEditor.Animations;
 
 public class GameController : MonoBehaviour {
     public GameObject player1;
     public GameObject player2;
-    public AnimatorController Orange;
-    public AnimatorController blue;
+    public RuntimeAnimatorController Orange;
+    public RuntimeAnimatorController blue;
     // Use this for initialization
     void Awake () {
         
         if (PlayerPrefs.GetString("Player1").Equals("Orange"))
         {
-            player1.GetComponent<Animator>().runtimeAnimatorController = Orange as RuntimeAnimatorController;
+            player1.GetComponent<Animator>().runtimeAnimatorController = Orange;
         }
         else if (PlayerPrefs.GetString("Player1").Equals("Blue"))
         {
-            player1.GetComponent<Animator>().runtimeAnimatorController = blue as RuntimeAnimatorController;
+            player1.GetComponent<Animator>().runtimeAnimatorController = blue;
         }
         if (PlayerPrefs.GetString("Player2").Equals("Orange"))
         {
-            player2.GetComponent<Animator>().runtimeAnimatorController = Orange as RuntimeAnimatorController;
+            player2.GetComponent<Animator>().runtimeAnimatorController = Orange;
         }
         else if (PlayerPrefs.GetString("Player2").Equals("Blue"))
         {
             print("I am here father");
-            player2.GetComponent<Animator>().runtimeAnimatorController = blue as RuntimeAnimatorController;
+            player2.GetComponent<Animator>().runtimeAnimatorController = blue;
         }
     }
 	
